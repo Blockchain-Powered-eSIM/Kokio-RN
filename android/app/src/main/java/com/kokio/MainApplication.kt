194efada42c77efc07ece0d6e11b3171d58fc4ae
GuyPhy <guyphy@kokio.app>
2024-08-12 15:54:36 +0530

    Add ReactNative module to interface with EUICC Manager API

--- android/app/src/main/java/com/kokio/MainApplication.kt
+++ android/app/src/main/java/com/kokio/MainApplication.kt
@@ -6,10 +6,14 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+
+import com.kokio.euiccBridge.euiccBridgePackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -19,6 +23,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+              add(euiccBridgePackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
---
+++ android/app/src/main/java/com/kokio/euiccBridge/euiccBridgeModule.java
@@ -0,0 +1,66 @@
+package com.kokio.euiccBridge;
+
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.telephony.euicc.EuiccManager;
+import android.util.Log;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
+
+public class euiccBridgeModule extends ReactContextBaseJavaModule {
+
+    private final static String TAG = euiccBridgeModule.class.getCanonicalName();
+    private final static String E_NO_CARRIER_NAME = "no_carrier_name";
+    private final static String E_NO_ISO_COUNTRY_CODE = "no_iso_country_code";
+    private final static String E_NO_MOBILE_COUNTRY_CODE = "no_mobile_country_code";
+    private final static String E_NO_MOBILE_NETWORK = "no_mobile_network";
+    private final static String E_NO_NETWORK_OPERATOR = "no_network_operator";
+    private final static String E_NO_EID = "no_eid_available";
+    private final static String E_NO_CARRIER_PRIVILEGES = "no_carrier_privilege_available";
+    private static ReactApplicationContext mReactContext;
+    private TelephonyManager mTelephonyManager;
+    private EuiccManager mEuiccManager;
+
+    euiccBridgeModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        mTelephonyManager = (TelephonyManager) reactContext.getSystemService(Context.TELEPHONY_SERVICE);
+
+        mReactContext = reactContext;
+        initEuiccManager();
+    }
+
+    private void initEuiccManager() {
+        if (mEuiccManager == null) {
+            mEuiccManager = (EuiccManager) mReactContext.getSystemService(Context.EUICC_SERVICE);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "euiccBridge"; // Name exposed to React Native
+    }
+
+    // Getting the EID
+    @ReactMethod 
+    public void getEID(Promise promise) {
+        //Log.d(TAG, "Carrier Privilege State is:" + mTelephonyManager.hasCarrierPrivileges());
+        try {
+            initEuiccManager();
+            if(mTelephonyManager.hasCarrierPrivileges()){
+              if (mEuiccManager.isEnabled()) {
+                  String eid = mEuiccManager.getEid();
+                  promise.resolve(eid);
+              } else {
+                  promise.reject(E_NO_EID, "eUICC Manager is not enabled");
+              }
+            } else {
+                promise.reject(E_NO_CARRIER_PRIVILEGES, "hasCarrierPrivileges check failed");
+            }
+        } catch (Exception e) {
+            promise.reject("Error", e.getMessage()); 
+        }
+    }
+}
