dcc783545d6969197536e39977c19a90cd6b525e
Rowdy <mroddy5280@gmail.com>
2024-08-23 16:30:44 -0600

    fix(prettier-plugin-tailwindcss): fixed configs for prettier and prettier plugin tailwindcss

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "plugins": ["prettier-plugin-tailwindcss"],
+  plugins: ['prettier-plugin-tailwindcss'],
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
